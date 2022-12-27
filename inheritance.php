0506d9f2afb922fc0e67ff8e8ab22b46935745ae
darulannas <darulannas7@gmail.com>
2022-12-27 23:42:25 +0700

    inheritance and overriding

---
+++ inheritance.php
@@ -0,0 +1,86 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+// ini class
+class Produk {
+    //visibility    //property
+    public  $judul, 
+            $penulis,
+            $penerbit,
+            $harga,
+            $jmlHalaman,
+            $waktuMain;
+
+                    //constructor
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+            //method
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+}
+
+            //inheritance
+class Komik extends Produk {
+    public function getInfoProduk() {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+            //inheritance
+class Game extends Produk {
+    public function getInfoProduk() {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+//object type
+class CetakInfoProduk {
+    public function cetak (Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+// //ini instance/object
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+
+
+// Komik : Masashi Kishimoto, Shonen Jump
+// Game : Neil Druckman, Sony Computer
+// Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
+
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+// Game : Uncharted | Neil Duckman, Sony Computer (Rp. 2500.000) - 50 Jam.
---
+++ overriding.php
@@ -0,0 +1,104 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+// ini class
+class Produk {
+    //visibility    //property
+    public  $judul, 
+            $penulis,
+            $penerbit,
+            $harga;
+
+                    //constructor
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+            //method
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+}
+
+            //inheritance
+class Komik extends Produk {
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+
+            //overriding
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jmlHalaman = $jmlHalaman;
+
+    }
+
+    public function getInfoProduk() {
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+        return $str;        //overriding
+    }
+}
+
+            //inheritance
+class Game extends Produk {
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
+
+            //overriding
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->waktuMain = $waktuMain;
+
+    }
+
+    public function getInfoProduk() {
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+//object type
+class CetakInfoProduk {
+    public function cetak (Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+// //ini instance/object
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+
+
+// Komik : Masashi Kishimoto, Shonen Jump
+// Game : Neil Druckman, Sony Computer
+// Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
+
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+// Game : Uncharted | Neil Duckman, Sony Computer (Rp. 2500.000) - 50 Jam.
