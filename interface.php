81a089ad8ee5c0082084c8d24ffb663b42e894ef
darulannas <darulannas7@gmail.com>
2022-12-30 23:34:05 +0700

    add interface.php

---
+++ interface.php
@@ -0,0 +1,180 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+//interface
+interface InfoProduk {
+    public function getInfoProduk();
+}
+
+// ini class
+//abstract
+abstract class Produk {
+    //visibility    //property
+    protected  $judul, 
+             $penulis,
+             $penerbit,
+             $harga,
+             $diskon = 0;
+
+                    //constructor
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+        //setter
+    public function setJudul($judul) {
+        if (!is_string($judul)) {
+            throw new Exception("Judul harus string");
+        }
+        $this->judul = $judul;
+    }
+
+    public function setPenulis($penulis) {
+        $this->penulis = $penulis;
+    }
+
+    public function setPenerbit($penerbit) {
+        $this->penerbit = $penerbit;
+    }
+
+    public function setHarga($harga) {
+        $this->harga = $harga;
+    }
+
+    public function setDiskon($diskon) {
+        $this->diskon = $diskon;
+    }
+
+        //getter
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
+    public function getHarga() {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function getDiskon() {
+        return $this->diskon;
+    }
+
+            //method
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+    
+    abstract public function getInfo();
+    
+}
+
+            //inheritance
+class Komik extends Produk implements InfoProduk {
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+
+            //overriding
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jmlHalaman = $jmlHalaman;
+
+    }
+
+    public function getInfo() {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+
+    public function getInfoProduk() {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+        return $str;        //overriding
+    }
+}
+
+            //inheritance
+class Game extends Produk implements InfoProduk {
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
+
+            //overriding
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->waktuMain = $waktuMain;
+
+    }
+
+    public function getInfo() {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+
+    public function getInfoProduk() {
+        $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+//object type
+class CetakInfoProduk {
+    public $daftarProduk = array();
+
+    public function tambahProduk( Produk $produk ) {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak() {
+        $str = "DAFTAR PRODUK : <br>";
+
+        foreach( $this->daftarProduk as $p ) {
+            $str .= "- {$p->getInfoProduk()} <br>";
+        }
+
+        return $str;
+    }
+}
+
+// //ini instance/object
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
+
+
+
+
+
+// Komik : Masashi Kishimoto, Shonen Jump
+// Game : Neil Druckman, Sony Computer
+// Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
+
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30.000) - 100 Halaman.
+// Game : Uncharted | Neil Duckman, Sony Computer (Rp. 2500.000) - 50 Jam.
